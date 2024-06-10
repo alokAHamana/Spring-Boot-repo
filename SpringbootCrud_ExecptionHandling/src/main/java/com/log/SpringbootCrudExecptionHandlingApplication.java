36e486837440aaf5c17aeb47350861fdd66cf26b
Alok <alokahamana@gmail.com>
2024-06-10 17:35:17 +0530

    exception commited to repo

---
+++ SpringbootCrud_ExecptionHandling/src/main/java/com/log/SpringbootCrudExecptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudExecptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudExecptionHandlingApplication.class, args);
+	}
+
+}
